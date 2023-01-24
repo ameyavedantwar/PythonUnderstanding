22924800d42e93a416b5c2609ea032e67554ea75
ameyavedantwar <115992039+ameyavedantwar@users.noreply.github.com>
2023-01-24 15:57:16 +0000

    Add files via upload

---
+++ Assignment 1.py
@@ -0,0 +1,16 @@
+print('hello')
+a = 2 + 3
+b = 10/5
+print(a,b)
+#Formula for triangle 1/2*base*height
+b = 10
+ht = 5
+area = (b*ht)*0.5
+print(area)
+
+a,b = b,a
+print(a,b)
+
+import random
+a = random.randint(1,1000)
+print(a)
